c638a8fec90648d658b1cd4f76025792bd10d71f
bhavesh2811shah <109756920+bhavesh2811shah@users.noreply.github.com>
2023-12-06 10:39:49 +0530

    update

---
+++ project_1/src/products.js
@@ -0,0 +1,10 @@
+export const PRODUCTS = [
+    {
+      id: 1,
+      productName: "IPhone",
+      price: 999.0,
+      url:"https://images-eu.ssl-images-amazon.com/images/I/514C7oI3dCL._AC_UL160_SR160,160_.jpg"
+    }
+]
+
+
